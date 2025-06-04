6855b63f091035946f1267cb53955b6767d2b3ed
Elite_Ultimate <138756197+Phuthanet25650023@users.noreply.github.com>
2025-06-04 14:55:48 +0700

    Update LAB2_ADC.c

--- LAB2_ADC.c
+++ LAB2_ADC.c
@@ -1,3 +1,29 @@
+/////////////////////////////////////LAB2_0////////////////////////////////////
+int LED_PIN = 16;
+int vol=12;
+float k1=0.98,volume=0;
+void filter_sink_Voltage_V(float value) {
+  volume = ((value) * (1 - k1)) + volume * k1;
+} //Low Pass Filter
+
+void setup() {
+  Serial.begin (115200); // เริ่มต้น Serial Communication เพื่อดูค่าบน Serial Monitor ที่ baud rate 115200
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(vol,INPUT);// ตั้งค่า GPIO 34 เป็น INPUT สำหรับอ่านค่า Analog
+
+}
+void loop() {
+  filter_sink_Voltage_V(analogRead(vol)); // ค่าอยู่ในช่วง 0 - 4095 (12-bit)
+  int Data_volume= map(volume, 0, 4095, 0, 255); // // แปลงจาก 12 บิต เป็น 8 บิต
+  analogWrite(LED_PIN,Data_volume); 
+  Serial.print("Raw ADC: ");
+  Serial.print(volume);
+  Serial.print(" | Mapped: ");
+  Serial.println(Data_volume);
+  delay(100); // รอ 1 วินาที
+  
+}
+///////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////// LAB2_1////////////////////////////////////
 int LED_PIN[] = {19, 18, 5, 17 ,16};
 int vol=34;
