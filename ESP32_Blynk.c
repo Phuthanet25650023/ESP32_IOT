da75cf4f1bdbb7d62582bf56a851ae33f2b945e7
Elite_Ultimate <138756197+Phuthanet25650023@users.noreply.github.com>
2025-07-22 20:30:53 +0700

    Update ESP32_Blynk.c

--- ESP32_Blynk.c
+++ ESP32_Blynk.c
@@ -172,3 +172,65 @@ void loop() {
   timer.run();
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////// LAB 03 //////////////////////////////////////////////////////////////////////////
+#define BLYNK_TEMPLATE_ID "TMPL6Wkr4Qlj5"
+#define BLYNK_TEMPLATE_NAME "Phuthanet"
+#define BLYNK_AUTH_TOKEN "AJDJjFnxey_4JOmVdWeT2fvk26cYky0T"
+
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// WiFi info
+char ssid[] = "Elite_Ultimate_2.4G";
+char pass[] = "24776996";
+
+// Pin map
+const int pin15 = 15;
+const int pin16 = 16;
+const int pin17 = 17;
+
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  pinMode(pin15, OUTPUT);
+  pinMode(pin16, OUTPUT);
+  pinMode(pin17, OUTPUT);
+}
+
+// Button on V0 → GPIO 15
+BLYNK_WRITE(V0)
+{
+  int value = param.asInt();
+  digitalWrite(pin15, value);
+  Serial.println("Switch 0 Is Active");
+  Serial.println(value);
+}
+
+// Button on V1 → GPIO 16
+BLYNK_WRITE(V1)
+{
+  int value = param.asInt();
+  digitalWrite(pin16, value);
+  Serial.println("Switch 1 Is Active");
+  Serial.println(value);
+}
+
+// Button on V2 → GPIO 17
+BLYNK_WRITE(V2)
+{
+  int value = param.asInt();
+  digitalWrite(pin17, value);
+  Serial.println("Switch 2 Is Active");
+  Serial.println(value);
+}
+
+void loop()
+{
+  Blynk.run();
+}
